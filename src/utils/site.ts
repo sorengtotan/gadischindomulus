40a82290985a57b9fb3ff732ee983f90d57b661f
sorengtotan <sorengtotan@gmail.com>
2025-07-15 15:41:10 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Gadis Chindo Mulus';
-export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Live Viral Colmek Sma Cindo Tante China';
-export const url = 'https://gadischindomulus.pages.dev';
-export const terbit = '2025-01-27T00:01:03Z';
